b9507fcd8eca9edb9f502a4e5e3421783934bf5d
berrondo <claudio.berrondo@gmail.com>
2020-08-22 05:51:15 +0000

    Django 1.6

--- dojopuzzles/dojopuzzles/settings.py
+++ dojopuzzles/dojopuzzles/settings.py
@@ -31,6 +31,8 @@ DATABASES = {
     # }
 }
 
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
